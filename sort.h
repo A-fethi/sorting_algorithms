4038dbd0ffe8b645547594a013b5774e781a3908
CANDELY001 <candely1charifa@gmail.com>
2023-07-20 15:18:22 -0700

    Counting sort

--- sort.h
+++ sort.h
@@ -28,4 +28,5 @@ void	insertion_sort_list(listint_t **list);
 void	shell_sort(int *array, size_t size);
 void cocktail_sort_list(listint_t **list);
 void counting_sort(int *array, size_t size);
+
 #endif
